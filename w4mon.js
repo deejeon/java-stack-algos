83d5b177ed38edca4863db20948ab210184bbb60
deejeon <61816883+deejeon@users.noreply.github.com>
2020-06-23 10:12:04 -0700

    added w4 tue

--- w4mon.js
+++ w4mon.js
@@ -1,4 +1,5 @@
-//Daniel, Sam, Jerry,Dathan
+// Daniel, Niv, Jerry
+
 class BSTNode {
     constructor(val) {
         this.val = val;
@@ -7,18 +8,71 @@ class BSTNode {
     }
 }
 
-
-
-// [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
-//                                         x
-// [14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
-//                       x
-// [14, 15, 16, 17, 18]
-//          x
-
 class BST {
     constructor() {
         this.root = null;
+    }
+
+    // return true / false if the value exists within the given tree
+    // if no tree is given, start a this.root
+    find(val, node = this.root){
+        if(node.val === val){
+            return true;
+        }
+        if (val<node.val){
+            if(node.left===null){
+                return false;
+            }
+            else {
+                return this.find(val, node.left);
+            }
+        }
+        if (val>node.val){
+            if(node.right===null){
+                return false;
+            }
+            else{
+                return this.find(val, node.right);
+            }
+        }
+    }
+
+    // remove and return the smallest node of a given tree
+    removeSmallest(node=this.root, prevNode=null){
+        if (this.isEmpty()){
+            return null;
+        }
+        if (this.root.left === null){
+            this.root = this.root.right;
+            node.right = null;
+            return node;
+        }
+        if (node.left === null){
+            prevNode.left = node.right;
+            node.right = null;
+            return node;
+        } else {
+            return this.removeSmallest(node.left, node);
+        }
+    }
+
+    // remove and return the largest node of a given tree
+    removeLargest(node=this.root, prevNode=null){
+        if (this.isEmpty()){
+            return null;
+        }
+        if (this.root.right === null){
+            this.root = this.root.left;
+            node.left = null;
+            return node;
+        }
+        if (node.right === null){
+            prevNode.right = node.left;
+            node.left = null;
+            return node;
+        } else {
+            return this.removeLargest(node.right, node);
+        }
     }
 
     isEmpty(){
@@ -26,49 +80,77 @@ class BST {
     };
 
     insert(node, tree){
-        if(this.root == null){
+        if(tree === undefined){
+            tree = this.root;
+        };
+
+        if(tree === null){
             this.root = node;
             return;
-        }
-        if(node.val > tree.val){
-            if (tree.right === null) {
-                tree.right = node;
-                return;
-            }
-            this.insert(node, tree.right);
-        }
-        if(node.val < tree.val){
-            if (tree.left === null) {
+        };
+
+        // go left
+        if(tree.val > node.val){
+            // check if null and add
+            if(tree.left === null){
                 tree.left = node;
                 return;
+            }else{
+                // else recurse left
+                return this.insert(node, tree.left);
             }
-            this.insert(node, tree.left);
+        // else go right
+        }else if(tree.val < node.val){
+            // check if null and add
+            if(tree.right === null){
+                tree.right = node;
+                return;
+            }else{
+                // else recurse right
+                return this.insert(node, tree.right);
+            }
         }
     };
 
-    getLargestFromSubtree(node){
-        if(node == null){
+    getLargestFromSubtree(tree){
+        if(tree === undefined){
+            tree = this.root;
+        }
+
+        // ... cont
+    };
+
+    getSmallestFromSubtree(tree){
+        // if no tree, tree is root
+        if(tree === undefined){
+            tree = this.root;
+        }
+
+        // if tree becomes null, return null
+        if(tree === null){
             return null;
         }
-        if(node.right === null) return node.val;
-        return this.getLargestFromSubtree(node.right);
-    }
 
-    getSmallestFromSubtree(node){
-          if(node == null){
-              return null;
-          }
-          if(node.left === null) return node.val;
-          return this.getSmallestFromSubtree(node.left);
-      }
+        // if there is no further nodes, return tree
+        if(tree.left === null){
+            return tree;
+        }
+
+        // else recurse to the left and try again
+        return this.getSmallestFromSubtree(tree.left);
+    };
 }
 
-
 var tree = new BST();
-tree.insert(new BSTNode(10), tree.root);
+// tree.insert(new BSTNode(10), tree.root);
 tree.insert(new BSTNode(111), tree.root);
 tree.insert(new BSTNode(3), tree.root);
 tree.insert(new BSTNode(1), tree.root);
-// console.log(tree);
-console.log(tree.getLargestFromSubtree(tree.root));
-// https://www.cs.usfca.edu/~galles/visualization/BST.html
+tree.insert(new BSTNode(2), tree.root);
+console.log(tree);
+
+console.log(tree.removeSmallest());
+console.log(tree);
+
+console.log(tree.removeLargest());
+console.log(tree);
